0e2dc81d7f6604e91f765d74fb8a40ddd6bac38b
Denis Ogun <do1303@my.bristol.ac.uk>
2014-03-15 15:28:33 +0000

    Send 404 page

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 app.configure(function () {
-    app.use(
-        "/", //the URL throught which you want to access to you static content
-        express.static(__dirname) //where your static content is located in your filesystem
-    );
+	app.use(express.compress());
+    app.use("/", express.static(__dirname));
+
+    app.use(function(req, res, next){
+  		res.status(404).sendfile('404.html');
+	});
 });
 app.listen(4000); //the port you want to use
 

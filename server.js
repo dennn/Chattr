559f0fcb36de85cdd8504933e95e6c65699b18ed
Denis Ogun <do1303@my.bristol.ac.uk>
2014-03-16 15:15:04 +0000

    Updated to XHTML

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 app.configure(function () {
 	app.use(express.compress());
+	app.use(function(req, res, next) {
+    	res.setHeader("Content-Type", "application/xhtml+xml");
+    	return next();
+ 	});
     app.use("/", express.static(__dirname));
 
     app.use(function(req, res, next){

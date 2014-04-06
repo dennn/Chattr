e983829f722b19bb77e3734ea8998402b8d7f7fa
Denis Ogun <do1303@my.bristol.ac.uk>
2014-04-06 14:54:12 +0100

    attempt to fix redis issue

--- config/passport.js
+++ config/passport.js
@@ -34,6 +34,7 @@ module.exports = function(passport, redis) {
 
 	function(token, refreshToken, profile, done) {
 		process.nextTick(function() {
+			console.log(profile);
 			var userString = "users:" + profile.id;
 			redis.hgetall(userString, function(err, user) {
 				if (err) {
